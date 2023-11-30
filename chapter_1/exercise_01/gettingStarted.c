7f927fc06d5b759e3f8397285b5233123890ca06
x86Tetra <olle.dunker@protonmail.com>
2023-11-30 09:42:38 +0100

    Completion of exercises in chapter 1.1

---
+++ chapter_1/exercise_01/gettingStarted.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+// Note: It appears that if we don't declare any datatype of our main function,
+// it'll default to the datatype of an integer; alongside a warning of implicit return types.
+main()
+{
+	printf("Hello, World!\n");
+}
---
+++ chapter_1/exercise_02/gettingStarted.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+	// Output: Hello,c World!
+	printf("Hello,\c World!\n");
+}
