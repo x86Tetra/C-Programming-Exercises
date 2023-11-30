a3fdcd80c119600918f249e398dd1219c0dba268
x86Tetra <olle.dunker@protonmail.com>
2023-11-30 14:07:51 +0100

    Updated exercise 02

--- chapter_1/exercise_02/gettingStarted.c
+++ chapter_1/exercise_02/gettingStarted.c
@@ -2,6 +2,13 @@
 
 int main()
 {
-	// Output: Hello,c World!
-	printf("Hello,\c World!\n");
+	printf("Hello, World!\e");
+	printf("Hello, World!\z");
+	printf("Hello, World!\p");
+	// Output: Hello, World!ello, World!zHello, World!p
+
+	// Escape sequences starts with a backslash, and hence
+	// it will "ignore it" if it's an invalid such (observe
+	// the output of \z and \p). To actually create a backspace,
+	// you'd have to use a double backslash ( \\ ).
 }
