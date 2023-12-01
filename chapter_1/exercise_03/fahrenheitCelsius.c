c17b534360ccaddb4033ba9a7a76f2fedcaf7159
x86Tetra <olle.dunker@protonmail.com>
2023-12-01 13:02:56 +0100

    Relatively well documented

--- chapter_1/exercise_03/fahrenheitCelsius.c
+++ chapter_1/exercise_03/fahrenheitCelsius.c
@@ -4,6 +4,9 @@
 	by the following variables: lowerLimit, upperLimit and step.
 */
 
+// stdio.h is a standard library for input and output operations. Operations like these
+// are not included in the core C programming language by itself; but are implemented by a
+// C library, such as glibc or musl.
 #include <stdio.h>
 
 // As learnt before, we should be explicit with the return type;
@@ -22,11 +25,19 @@ int main()
 	// The condition in the parantheses is tested; if it's true (that
 	// being if fahr is lesser than or equal to upperLimit), the code
 	// encapsulated in the while loop will be executed.
-
 	while (fahr <= upperLimit)
 	{
+		// In C, integer division truncates; any franctional part is
+		// discarded. Meaning that a statement involving a multiplication
+		// with (5/9) would just truncate down to 0, hence the output would
+		// be 0 for all iterations.
 		celsius = 5 * (fahr-32) / 9;
+		// Printf(); is a general purpose output formatting function.
+		// The first argument specifies a string of characters, where each % construction
+		// is paired with their corresponding secondary and so forth arguments.
+		// As for %d, it specifies an integer argument to be printed.
 		printf("%d\t%d\n", fahr, celsius);
+		// Increase the value in the "fahr" variable with the specified value in "step".
 		fahr += step;
 	}
 }
