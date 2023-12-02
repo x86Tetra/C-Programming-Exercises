51ca0165ccfcbf1859d75ccb1b1d887dbb366978
x86Tetra <olle.dunker@protonmail.com>
2023-12-02 17:23:41 +0100

    Improved comments

--- chapter_1/exercise_05/fahrenheitCelsiusCompactReversed.c
+++ chapter_1/exercise_05/fahrenheitCelsiusCompactReversed.c
@@ -12,10 +12,21 @@ int main()
 	// the second argument tests a certain condition as defined by the constants,
 	// and the third specifies the decrement (being 20 in this scenario).
 
-	// Per se, the for construct consists of 3 different arguments that must be
+	// Per se, the "for" construct consists of 3 different arguments that must be
 	// specified, these arguments are in the following order: initialization,
 	// condition and increment/decrement; but can generally be any expression.
 
+	// If the condition resolves to true, the body of the loop will be executed and an
+	// increment/decrement occurs shortly after. If it resolves to false,
+	// it will exit the loop and continue operation (which is nothing in this case,
+	// hence resulting in a program termination).
 	for (int fahr = 300; fahr >= 0; fahr -= 20)
 		printf("%3dF = %6.1fC\n", fahr, (5.0/9.0)*(fahr-32));
+
+	// As we can observe from above, we've put the arithmetic expression as the third
+	// argument in the printf statement. This is completely valid due to a general
+	// rule in C; where you can use a variable of a datatype, you can perform a
+	// complicated expression there as well. And as we've covered before, any
+	// floating point expression may be performed in the third argument due to
+	// the % construct being of a floating point nature.
 }
