69fc7868ef8441db8c7df29412914b0f90fa600b
x86Tetra <olle.dunker@protonmail.com>
2023-12-06 13:55:04 +0100

    Added comments

--- chapter_1/exercise_10/basicEscapeSequenceViewer.c
+++ chapter_1/exercise_10/basicEscapeSequenceViewer.c
@@ -1,3 +1,16 @@
+/*
+	Instructions: Write a program to copy its input to output,
+	replacing each tab by \t, each backspace by \b, and each
+	backslash with \\. This makes tabs and backspaces visible
+	in an unambiguous way.
+
+	Comments: Understanding this should be straightforward,
+	considering what have been written beforehand. We test
+	each character in a text stream for any characters that
+	would need to be formatted; backspaces, tabs and backslashes.
+*/
+
+
 #include <stdio.h>
 
 int main()
