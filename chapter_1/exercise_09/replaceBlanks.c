90dab9d1316a5fbbef7b0889234527e804c5722e
x86Tetra <olle.dunker@protonmail.com>
2023-12-06 13:16:09 +0100

    Added comments

--- chapter_1/exercise_09/replaceBlanks.c
+++ chapter_1/exercise_09/replaceBlanks.c
@@ -1,3 +1,8 @@
+/*
+	Instructions: Write a program to copy its input to its output,
+	replacing each string of one or more blanks by a single blank.
+*/
+
 #include <stdio.h>
 
 
@@ -7,11 +12,20 @@ int main()
 
 	while ((c = getchar()) != EOF)
 	{
+		// If the newest character (c) and the recently used character
+		// equals a blank, do nothing and wait for the next character.
+		// In C, at least the grammatical rules of C,
+		// it is required for statements to have a body.
+		// However, a null statement, being the isolated
+		// semicolon, can be utilized to satisfy that
+		// requirement; as seen below the statement to
+		// emphansize readability.
 		if (c == ' ' && lastCharacter == ' ')
 			;
+		// Otherwise, print the newest character from the text stream.
 		else
 			putchar(c);
-
+		// Save the recent character as the last character.
 		lastCharacter = c;
 	}
 
