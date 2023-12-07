599a97fae2bb47fb55a7937b5ce3ca33209b7e56
x86Tetra <olle.dunker@protonmail.com>
2023-12-07 19:14:50 +0100

    Initial creation

---
+++ chapter_1/exercise_11/wordCount.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+#define IN 	1
+#define OUT 	0
+
+int main()
+{
+	int c, nl, nw, nc, state;
+
+	state = OUT;
+	nl = nw = nc = 0;
+
+	while ((c = getchar()) != EOF)
+	{
+		++nc;
+		if (c == '\n')
+			++nl;
+		if (c == ' ' || c == '\n' || c == '\t')
+			state = OUT
+		else if(state == OUT)
+		{
+			state == IN;
+			++nw;
+		}
+	}
+}
+
+/*
+	Question: How would you test the word count program, what
+	kinds of input are most likely to uncover bugs if there are any.
+*/
+
