097d12df4759ff6b802b0b5b13b9156156428ea9
x86Tetra <olle.dunker@protonmail.com>
2023-12-08 11:08:24 +0100

    Answered the questions in exercise 11

--- chapter_1/exercise_11/wordCount.c
+++ chapter_1/exercise_11/wordCount.c
@@ -1,4 +1,4 @@
- /*
+/*
 	Program description: a very barebone and minimalist version
 	of the commonly used UNIX command: wc (word counter).
 */
@@ -36,4 +36,23 @@ int main()
 /*
 	Question: How would you test the word count program, what
 	kinds of input are most likely to uncover bugs if there are any.
+
+	Answer:	I would test it with several types of input, that being:
+
+	-> No input
+
+	-> Single characters
+
+	-> Double characters
+
+	-> Multi-character characters (such as 'å', 'ä' and 'ö' in
+	the Swedish alphabet)
+
+	-> Words seperated with blanks and tabs
+
+	-> Multi-line input containing multiple words.
+
+	Input that could arise bugs are those that tests boundary conditions,
+	like the input exceeding the limits of the signed integer we are using;
+	possibly generating a negative number in nl, nw, nc.
 */
