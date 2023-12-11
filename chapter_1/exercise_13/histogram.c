56695752b12d384500c3a344889f00fbe8bb7e72
x86Tetra <olle.dunker@protonmail.com>
2023-12-11 22:16:49 +0100

    Initial creation of exercise 13

---
+++ chapter_1/exercise_13/histogram.c
@@ -0,0 +1,57 @@
+/*
+	Instruction: Write a program to print a histogram of
+	the lengths of words in its input.
+
+	TODO: comment further
+*/
+
+
+
+#include <stdio.h>
+
+#define OUT 0
+#define IN  1
+
+#define MAX_LENGTH 12
+
+int main()
+{
+
+	int count[MAX_LENGTH];
+	int c, state, length = 0;
+
+	for (int i = 0; i < MAX_LENGTH+1; i++)
+		count[i] = 0;
+
+	while ((c = getchar()) != EOF)
+	{
+		if (c == ' ' || c == '\t' || c == '\n')
+		{
+			state = IN;
+
+			if (length > MAX_LENGTH)
+				count[MAX_LENGTH+1]++;
+			else if (length <= 0)
+				;
+			else
+				count[length-1]++;
+		}
+		else if (state == IN)
+		{
+			length = 0;
+			state = OUT;
+		}
+		else
+			length++;
+	}
+
+
+	for (int i = 0; i < MAX_LENGTH; i++)
+	{
+		printf("\n%d\t: ", i+1);
+		for (int j = 0; j < count[i]; j++)
+			printf("#");
+	}
+
+	printf("\n");
+}
