914d34d31598e50f38130b1d570bc0c7e9ed0ea6
x86Tetra <olle.dunker@protonmail.com>
2023-12-12 22:19:10 +0100

    Initial creation of exercise 14

---
+++ chapter_1/exercise_14/histogram.c
@@ -0,0 +1,36 @@
+/*
+	Instructions: Write a program to print a histogram of the frequencies
+	of different characters in its input.
+*/
+
+
+#include <stdio.h>
+
+#define LENGTH 0b1111111
+
+int main()
+{
+	int c;
+	int array[LENGTH];
+
+	for (int i = 0; i < LENGTH; i++)
+		array[i] = 0;
+
+	while((c = getchar()) != EOF)
+	{
+		if (c <= LENGTH)
+			array[c]++;
+	}
+
+	for (int i = 0; i < LENGTH; ++i)
+	{
+		if (array[i] > 0)
+		{
+			printf("Character %c (0x%02x): ", i, i);
+			for (int j = 0; j < array[i]; j++)
+				printf("#");
+			printf("\n");
+		}
+	}
+	printf("\n");
+}
