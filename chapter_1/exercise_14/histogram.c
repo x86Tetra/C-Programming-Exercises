96851e40a22829944f4119e830a1952479e726c0
x86Tetra <olle.dunker@protonmail.com>
2023-12-13 21:23:50 +0100

    Update code in exercise 14

--- chapter_1/exercise_14/histogram.c
+++ chapter_1/exercise_14/histogram.c
@@ -13,24 +13,21 @@ int main()
 	int c;
 	int array[LENGTH];
 
+	// Assign 0 to the integers in the array.
 	for (int i = 0; i < LENGTH; i++)
 		array[i] = 0;
 
 	while((c = getchar()) != EOF)
-	{
 		if (c <= LENGTH)
 			array[c]++;
-	}
 
 	for (int i = 0; i < LENGTH; ++i)
-	{
-		if (array[i] > 0)
+		if (array[i])
 		{
 			printf("Character %c (0x%02x): ", i, i);
 			for (int j = 0; j < array[i]; j++)
 				printf("#");
 			printf("\n");
 		}
-	}
 	printf("\n");
 }
