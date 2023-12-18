1697523994706145a71822602c494ae23a2bc879
x86Tetra <olle.dunker@protonmail.com>
2023-12-18 19:26:05 +0100

    Initial creation of exercise 17

---
+++ chapter_1/exercise_17/printLargerThan80.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+#define MAXLINE 1000
+#define CHARACTERS 80
+
+int main()
+{
+	char line[MAXLINE];
+	int length;
+
+	while ((length = fetchline(line, MAXLINE)) > 0)
+		if (length > CHARACTERS)
+			printf("%s", line);
+}
+
+int fetchline(char line[], int limit)
+{
+	int c, i, j = 0;
+
+	for (i = 0; (c = getchar()) != EOF && c != '\n'; i++)
+		if (i < limit-2)
+		{
+			line[j] = c;
+			j++;
+		}
+	if (c == '\n')
+	{
+		line[j] = c;
+		j++;
+	}
+	line[j] = '\0';
+	return i;
+}
