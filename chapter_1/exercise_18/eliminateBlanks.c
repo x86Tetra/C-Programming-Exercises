f2af6269da126fcc0ce426ce83186a76bbdd0b79
x86Tetra <olle.dunker@protonmail.com>
2024-02-03 17:46:45 +0100

    Completion

--- chapter_1/exercise_18/eliminateBlanks.c
+++ chapter_1/exercise_18/eliminateBlanks.c
@@ -5,18 +5,55 @@
 */
 
 #include <stdio.h>
-
-#include MAXLINE
+#define MAXLINE 10000
 
 int main()
 {
+  char text[MAXLINE];
+  while(fLine(text, MAXLINE) > 0)
+    if(removeContents(text) > 0)
+      printf("%s", text);
 }
 
-int fLine()
+int fLine(char line[], int limit)
 {
+  int c, i, j = 0;
+
+  for (i = 0; (c = getchar()) != EOF && c != '\n'; i++)
+    if (i < limit-2)
+    {
+      line[j] = c;
+      j++;
+    }
+  if (c == '\n')
+  {
+    line[j] = c;
+    i++;
+    j++;
+  }
+  line[j] = '\0';
+
+  return i;
 }
 
-int remove(char s[])
+int removeContents(char s[])
 {
+  int i = 0;
+  while(s[i] != '\n')
+    i++;
+
+  --i;
+
+  while(i >= 0 && (s[i] == ' ' || s[i] == '\t'))
+    --i;
+
+  if (i >= 0)
+  {
+    ++i;
+    s[i] = '\n';
+    ++i;
+    s[i] = '\0';
+  }
   
+  return i;
 }
