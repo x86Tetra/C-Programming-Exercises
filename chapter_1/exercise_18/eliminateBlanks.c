9501b304057c4b143df4dfb8bd0f7e38779c3c06
x86Tetra <olle.dunker@protonmail.com>
2024-02-03 16:04:25 +0100

    Creation of skeleton

--- chapter_1/exercise_18/eliminateBlanks.c
+++ chapter_1/exercise_18/eliminateBlanks.c
@@ -3,3 +3,20 @@
   blanks and tabs from each line of input, and to
   delete entirely blank lines.
 */
+
+#include <stdio.h>
+
+#include MAXLINE
+
+int main()
+{
+}
+
+int fLine()
+{
+}
+
+int remove(char s[])
+{
+  
+}
