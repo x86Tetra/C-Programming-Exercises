639e7300673045505cbdeac0a66764ecdd510677
x86Tetra <olle.dunker@protonmail.com>
2024-02-06 21:22:19 +0100

    Initial creation of exercise_20

---
+++ chapter_1/exercise_20/detab.c
@@ -0,0 +1,25 @@
+/* 
+ * Instructions: Write a program detab that replaces tabs in the input
+ * with the proper number of blanks to space to the next tab stop.
+ * Assume a fixed set of tab stops, say every n columns. Should
+ * n be a variable or a symbolic parameter.
+ */
+
+
+#include <stdio.h>
+
+int main()
+{
+
+}
+
+int fLine(char text[])
+{
+  int c, i, j = 0; 
+}
+
+int detab(char text[])
+{
+
+}
+
